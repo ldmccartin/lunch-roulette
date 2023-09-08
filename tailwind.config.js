d95020b6cd2afe433a045df0733cd2f040548418
Luke McCartin <ldmccartin@gmail.com>
2023-09-08 17:36:10 +0200

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,17 @@ import {nextui} from "@nextui-org/react";
 
 const config = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
       minWidth: {
-        450: '450px',
+        '450': '450px'
       }
-    },
+    }
   },
   darkMode: "class",
   plugins: [nextui()]
